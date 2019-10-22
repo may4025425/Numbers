00957d5574d76536bc01b465bbda79bc029741af
may4025425 <may4025425@gmail.com>
2019-10-22 18:11:28 +0800

    10/22

--- app/src/main/java/com/may/numbers/MainActivity.java
+++ app/src/main/java/com/may/numbers/MainActivity.java
@@ -1,38 +1,51 @@
 package com.may.numbers;
 
+import android.nfc.Tag;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.Random;
+
+
+//產生一到十的亂數，猜一次中good，猜大於五次太爛
 
 public class MainActivity extends AppCompatActivity {
 
 
+    String TAG = MainActivity.class.getSimpleName()  ;
     private TextView number;
     int counter;
+    int secret = new Random().nextInt(10)+1;
+    private ImageView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG,"secret"+secret);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         number = findViewById(R.id.number);
-
+        result = findViewById(R.id.result_image);
 
         FloatingActionButton fab = findViewById(R.id.fab);
-
-
 
         fab.setOnClickListener(new View.OnClickListener() {
 
@@ -40,11 +53,22 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-
-
+                //宣告變數
                 counter++;
                 number.setText(String.valueOf(counter));//記得選字串
+                result.setAlpha(1.0f);
+                result.setVisibility(View.VISIBLE);
 
+
+
+                //功能判斷
+                if(counter==secret){
+                    Toast.makeText(MainActivity.this,"you got it!!!",Toast.LENGTH_LONG).show();
+                    result.setImageResource(R.drawable.shocked);
+                }else {
+                    result.setImageResource(R.drawable.sadface);
+                    result.animate().alpha(0.0f).setDuration(1200);
+                }
 
 
 
